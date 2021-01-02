35594c1236567c598664b194c0d2c370a25cfa5a
@jonathanlei <jonathanlei98@gmail.com>
2021-01-01 22:46:10 -0500

    added get_listings function, to do URL parameters

--- airbnb.py
+++ airbnb.py
@@ -1,9 +1,7 @@
+""" scraping airbnb result page and return list of dictionaries about result listings"""
 import requests
 from bs4 import BeautifulSoup
 import re
-# """
-# URL = "http://api.proxiesapi.com"
-# auth_key = "60209c48efa989433e3f5c3924734a9c_sr98766_ooPq87" """
 
 
 url = """https://www.airbnb.com/s/Kerry--Ireland/homes?tab_id=home_tab&refinement_paths%5B%5D=%2F
@@ -11,45 +9,54 @@ homes&adults=1&checkin=2021-02-01&checkout=2021-03-01&source=structured_search_i
 &search_type=AUTOSUGGEST"""
 
 
-test_url = "https://www.airbnb.com/s/Paris--France/homes?refinementPaths%5B%5D=%2Fhomes&place_id=ChIJD7fiBh9u5kcRYJSMaMOCCwQ&adults=2&checkin=2021-01-07&checkout=2021-02-15&refinement_paths%5B%5D=%2Fhomes&source=structured_search_input_header&search_type=AUTOSUGGEST"
+test_url = "https://www.airbnb.com/s/Houston--TX--United-States/homes?checkin=2021-01-01&checkout=2021-01-08&adults=2&refinement_paths%5B%5D=%2Fhomes&source=structured_search_input_header&search_type=unknown&tab_id=home_tab&ne_lat=30.325413658688685&ne_lng=-94.9287617480469&sw_lat=27.527699030304657&sw_lng=-96.2416279589844&zoom=8&search_by_map=true"
 
 test_url2 = "https://www.airbnb.com/s/San-Francisco--CA--United-States/homes?adults=2&checkin=2021-01-07&checkout=2021-02-15&refinement_paths%5B%5D=%2Fhomes&source=structured_search_input_header&search_type=autocomplete_click&tab_id=home_tab&query=San%20Francisco%2C%20CA%2C%20United%20States&place_id=ChIJIQBpAG2ahYAR_6128GcTUEo"
 # PARAMS = {'auth_key': auth_key, 'url': url} 
 
-r = requests.get(url=test_url)
 
-soup = BeautifulSoup(r.content, 'html.parser')
+def get_single_listing_info(itemlist):
+    """ given a bs4 object(listing), get infos about a single listing """
+    listing_info = {}
 
-itemlist = soup.find(itemprop="itemListElement")
-# finding the div section
+    def find_div_section(itemlist):
+        """ find the first div that contains childs, the listing div"""
+        divs = itemlist.findChildren("div", {'class': re.compile("^_")})
+        for div in divs:
+            if len(div.findChildren()) > 1:
+                return div
+
+    div_section = find_div_section(itemlist)
+    # img_url
+    listing_info["img_url"] = div_section.findChild("img")["src"]
+    # rating
+    try:
+        listing_info["rating"] = (div_section.findChild("span", string=re.compile("^Rating"))
+                                  .getText()[7:10])
+    except AttributeError:
+        listing_info["rating"] = None
+    # total price
+    total_text = div_section.findChildren("button")[-1].text
+    total_index = total_text.index("total")
+    listing_info["total"] = total_text[:total_index+5].replace(u"\xa0", "")
+
+    # room type
+    listing_info["room_type"] = div_section.findChild("div", string=re.compile(" in ")).text
+
+    # description
+    listing_info["description"] = div_section.findChild("img")["alt"]
+
+    return listing_info
 
 
-def find_div_section(itemlist):
-    divs = itemlist.findChildren("div", {'class': re.compile("^_")})
-    for div in divs:
-        if len(div.findChildren()) > 0:
-            return div
+def get_listings_info(url):
+    """ given a airbnb url, scrape the page and return a list of listings div"""
+    r = requests.get(url=url)
 
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-div_section = find_div_section(itemlist)
-# img_url
-img_url = div_section.findChild("img")["src"]
-# rating
-rating_span = div_section.findChild("span", string=re.compile("^Rating")).getText()
-rating = rating_span[7:10]
+    itemlist = soup.find_all(itemprop="itemListElement")[:20]
 
-# #price per night 
-# price = div_section.findChild("span", string=re.compile("rice")).parent.text
-# dollar_index = price.index("$")
-# price = price[dollar_index:].replace(u"\xa0", "")
+    listings_info = [get_single_listing_info(listing) for listing in itemlist]
 
-# total price
-total_text = div_section.findChildren("button")[-1].text
-total_index = total_text.index("total")
-total = total_text[:total_index+5].replace(u"\xa0", "")
-
-# room type
-room_type = div_section.findChild("div", string=re.compile(" in ")).text
-
-# description
-description = div_section.findChild("img")["alt"]
+    return listings_info
